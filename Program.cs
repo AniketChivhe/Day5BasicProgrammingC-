4cae8de47d5d5bcd0823ebd0466c8912e485e1c6
“AniketChivhe” <aniketchivhe@gmail.com>
2022-05-11 01:26:33 +0530

    [Aniket]Problem1 is added

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_BasicProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Flip Coin Problem\n");
+            
+            int TotalFlip = TakeInput();
+            int TotalHead = CoinCheck(TotalFlip);
+            DisplayResult(TotalHead, TotalFlip);
+        }
+
+        public static int TakeInput()
+        {
+            Console.Write("Enter how many times to Flip: ");
+            string Num = Console.ReadLine();
+            return Convert.ToInt32(Num);
+        }
+
+        public static int CoinCheck(int TotalFlip)
+        {
+            Random random = new Random();
+            int HeadNum = 0;
+            while (TotalFlip > 0)
+            {
+                int CoinFlip = random.Next(0, 2);
+                if (CoinFlip == 0)
+                    HeadNum++;
+                TotalFlip--;
+            }
+            return HeadNum;
+        }
+
+        public static void DisplayResult(int TotalHead, int TotalFlip)
+        {
+            int TotalTail = TotalFlip - TotalHead;
+            Console.WriteLine("\nTotal number of Heads is: " + TotalHead + " and Tails is: " + TotalTail);
+            float HeadPercent = ((float)TotalHead / TotalFlip) * 100;
+            float TailPercent = ((float)TotalTail / TotalFlip) * 100;
+            Console.WriteLine("\nThus, the required percentage of Heads is: " + HeadPercent + "% and Tails is: " + TailPercent + "%");
+        }
+    }
+}
+
